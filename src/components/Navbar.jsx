d3cfd3d9515d46be5221330c010aaba49ebb85f6
AfazurRahman <afazurr8@gmail.com>
2026-06-27 14:13:44 +0600

    navbar fixed for mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -29,7 +29,7 @@ export default function Navbar() {
 
   return (
     <nav className="sticky top-0 z-40 w-full border-b border-separator bg-[#F8F8FF]">
-      <header className="mx-auto flex h-16 max-w-7xl items-center justify-between px-6 md:px-10 lg:px-5">
+      <header className="mx-auto flex h-16 max-w-7xl items-center justify-between px-3 md:px-10 lg:px-5">
         <div className="flex items-center gap-4">
           <button
             className="md:hidden"
